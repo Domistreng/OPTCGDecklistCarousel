8c31859a81d9a98d3665b26e92532356db5f32f0
Dominic Sitto <domisrotmg@gmail.com>
2025-04-18 11:29:26 -0400

    Modified readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# OPTCGDecklistCarousel
+# OPTCGDecklistDisplay
+### HTML + JS
+
+## This is designed to present a display of your one piece decklist as copied directly from the  [sim](https://www.dropbox.com/scl/fo/7mtqq7b0wemrsij8j7xbh/ALGfOqaenr_QTuGKA0Vf2vY?rlkey=k7xvkp0o6salh1034ga94l9r2&e=1&dl=0).
+
+## The guide [index.html](https://github.com/Domistreng/OPTCGDecklistDisplayJS/blob/main/index.html) will display usage + implementation of both multiple decklists wiht a scroller as well as a single deck list.
---
+++ opDeckListDisplay_singleDeck.js
@@ -0,0 +1,49 @@
+let tempParents = document.getElementsByClassName("opDiv")
+
+let tempParent = tempParents[0]
+// CHANGE: position: absolute;
+tempParent.style.backgroundColor = 'Black'
+tempParent.style.position = 'absolute'
+tempParent.style.top = '0px'
+tempParent.style.cursor = 'pointer';
+
+tempParent.onclick = function() {
+    navigator.clipboard.writeText(tempParent.getAttribute('deckList'));
+}
+
+// Need to make multiple heights for mobile vs pc screen
+tempParent.style.height= '700px'
+tempParent.style.width= '1240px' 
+deckList = tempParent.getAttribute('deckList').split(/\r?\n/);
+
+for (let i = 0; i < deckList.length; i++) {
+    let tempCont = document.createElement("div")
+    tempCont.style.position = "relative";
+    tempCont.style.textAlign = "center";
+    tempCont.style.display = 'inline-block';
+    
+
+
+    let temp = document.createElement("img")
+    temp.src="https://en.onepiece-cardgame.com/images/cardlist/card/" + deckList[i].substring(2) + ".png"
+    temp.style.height = '210px'
+    temp.style.width = '140px'
+    temp.style.marginTop = '10px'
+    temp.style.marginLeft = '5px'
+    temp.style.marginRight = '5px'
+
+    let tempText = document.createElement("div")
+    tempText.style.position = 'absolute';
+    tempText.style.top = '95%';
+    tempText.style.left = '50%';
+    tempText.style.transform = 'translate(-50%, -50%)';
+    tempText.style.fontSize = '40px';
+    tempText.style.color = 'White';
+    tempText.innerHTML = deckList[i].substring(0,2);
+
+    tempCont.appendChild(temp)
+    tempCont.appendChild(tempText)
+
+    tempParent.appendChild(tempCont)
+
+}
